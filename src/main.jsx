d0851dc9c3f454952b38b4bae6a2e79891a3639e
lucas11-ho <lacus.mm.ph@gmail.com>
2026-09-06 18:48:20 +0700

    Load A9.2 menu shortcut styles

--- src/main.jsx
+++ src/main.jsx
@@ -41,6 +41,7 @@ import './theme-product-typography-composer-a6.css';
 import './theme-commerce-surfaces-composer-a7.css';
 import './platform-icon-picker.css';
 import './category-icons-a9-1.css';
+import './menu-shortcuts-a9-2.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
