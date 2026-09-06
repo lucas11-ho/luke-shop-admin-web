584a67f754ed1edff9c74848b8201a4987379c4f
lucas11-ho <lacus.mm.ph@gmail.com>
2026-09-06 13:27:00 +0700

    Load reusable Platform Icon Picker styles

--- src/main.jsx
+++ src/main.jsx
@@ -38,6 +38,7 @@ import './theme-navigation-composer-a4.css';
 import './theme-controls-composer-a5.css';
 import './theme-product-typography-composer-a6.css';
 import './theme-commerce-surfaces-composer-a7.css';
+import './platform-icon-picker.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
